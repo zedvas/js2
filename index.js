3d711debc3b2f310eaeb60b938edba5c126d4cd6
zedvas <82768795+zedvas@users.noreply.github.com>
2024-05-29 16:42:31 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1.	Create a function that accepts two numbers, adds them and returns the result.
+// 2.	Write a function with the following behavior
+//      -    takes an array as a parameter
+//      -    reverses the order of the array
+//      -    returns the reversed array
+//  4. Create an array with 10 items
+//  5. Update one of the items
+//  6. Delete half of the array
+//  7. Write out each of the array methods so you become familiar with the syntax (use the array you made earlier)
+//    Pop
+//    Push
+//    Shift
+//    Unshift
+//    Concatenating
+//    Splicing
+//    Slicing
+//    Reverse
+//    Join
+//    forEach
+//    Includes
+//    indexOf
+//    Every
+//    Some
+//    Filter
+//    Find
+//    findIndex
+//    Sort
+//    Map
